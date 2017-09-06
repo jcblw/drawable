3a007b458ec5082169850a84dd4f6a334a37a364
Jacob Lowe <jacoblowe2.0@gmail.com>
2017-09-05 20:54:56 -0700

    Add object fit to images (#2)
    * add in objecFit implementation for images on canvas
    
    * add in documentation about object fit on image type
    
    * add in a bunch of image test

--- src/image.js
+++ src/image.js
@@ -1,4 +1,5 @@
 import Canvas from 'canvas';
+import sizeOf from 'image-size';
 const { loadImage } = Canvas;
 
 export default class DrawableImage {
@@ -9,25 +10,81 @@ export default class DrawableImage {
 
   loadImage() {
     return loadImage(this._img).then(image => {
+      this._dimensions = sizeOf(this._img);
       this._image = image;
     });
+  }
+
+  getRatio(width, height) {
+    return width / height;
+  }
+
+  getObjectFitDimensions(
+    { width: canvasWidth, height: canvasHeight },
+    objectFit
+  ) {
+    const { width, height } = this._dimensions;
+    const isCover = objectFit === 'cover';
+    const imageRatio = this.getRatio(width, height);
+    const canvasRatio = this.getRatio(canvasWidth, canvasHeight);
+    // contain is the cover algorithm flipped
+    const shouldUseCanvasHeight = isCover
+      ? canvasRatio < imageRatio
+      : canvasRatio >= imageRatio;
+    if (shouldUseCanvasHeight) {
+      const adjustedHeight = canvasHeight;
+      const adjustedWidth = canvasHeight * imageRatio;
+      return {
+        height: adjustedHeight,
+        width: adjustedWidth,
+        left: (adjustedWidth - canvasWidth) / 2 * -1,
+        top: 0,
+      };
+    } else {
+      const adjustedWidth = canvasWidth;
+      const adjustedHeight = canvasWidth / imageRatio;
+      return {
+        height: adjustedHeight,
+        width: adjustedWidth,
+        top: (adjustedHeight - canvasHeight) / 2 * -1,
+        left: 0,
+      };
+    }
   }
 
   getStyleProp(prop = '') {
     return this._styles[prop] || 0;
   }
 
-  draw(context) {
+  drawImage(context, canvasStyles) {
+    const objectFit = this._styles.objectFit;
+    let aligmentSpec;
+    if (objectFit) {
+      aligmentSpec = this.getObjectFitDimensions(canvasStyles, objectFit);
+    } else {
+      aligmentSpec = {
+        top: this.getStyleProp('top'),
+        left: this.getStyleProp('left'),
+        width: this.getStyleProp('width') || this._dimensions.width,
+        height: this.getStyleProp('height') || this._dimensions.height,
+      };
+    }
+    context.drawImage(
+      this._image,
+      aligmentSpec.left,
+      aligmentSpec.top,
+      aligmentSpec.width,
+      aligmentSpec.height
+    );
+  }
+
+  draw(context, canvasStyles) {
     if (!this._image) {
       return this.loadImage(this._img).then(() => {
-        context.drawImage(
-          this._image,
-          this.getStyleProp('left'),
-          this.getStyleProp('top')
-        );
+        this.drawImage(context, canvasStyles);
         return Promise.resolve();
       });
     }
-    context.drawImage(this._img, 0, 0);
+    this.drawImage(context, canvasStyles);
   }
 }
